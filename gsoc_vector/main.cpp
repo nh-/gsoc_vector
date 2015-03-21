2572798cc74c5525ea19f33e60b6a1cc266ddd8f
Nico Hartmann <mail@nicohartmann.de>
2015-03-21 23:04:07 +0100

    implemented
    - resize()
    - push_back()
    - at()
    - operator[]
    and support for custom allocators
    and tests for those functions

--- gsoc_vector/main.cpp
+++ gsoc_vector/main.cpp
@@ -1,12 +1,8 @@
 
 #include "vector.h"
+#include <array>
 #include <gtest/gtest.h>
 
-template<typename T>
-class fcv_test : public ::testing::Test
-{
-public:
-};
 
 struct NonDefaultConstructable
 {
@@ -18,21 +14,207 @@ struct NonDefaultConstructable
 	int x_;
 };
 
+struct NonCopyable
+{
+	NonCopyable() = default;
+private:
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable& operator=(const NonCopyable&) = delete;
+public:
+	NonCopyable(NonCopyable&& other) { }
+	NonCopyable& operator=(NonCopyable&& other) { return *this; }
+};
+
+template<typename T>
+T construct(int id = 0) { return T(); }
+template<>
+int construct<int>(int id) { return id; }
+template<>
+std::string construct<std::string>(int id) { return std::string(id, '~'); }
+// for types that are not default-constructible
+// template<>
+// NonDefaultConstructable construct<NonDefaultConstructable>()
+// {
+// 	return NonDefaultConstructable(1337);
+// }
+
+
+template<typename T>
+class fcv_basic_test : public ::testing::Test
+{
+public:
+};
+
 typedef ::testing::Types<
 	fixed_capacity_vector<int>, 
-	fixed_capacity_vector<std::string>,
-	fixed_capacity_vector<NonDefaultConstructable>
+// 	fixed_capacity_vector<void*>,
+// 	fixed_capacity_vector<const void*>,
+	fixed_capacity_vector<std::string>
+// 	fixed_capacity_vector<std::pair<float, double>>,
+// 	fixed_capacity_vector<NonDefaultConstructable>,
+// 	fixed_capacity_vector<NonCopyable>
 > TypesUnderTest;
-TYPED_TEST_CASE(fcv_test, TypesUnderTest);
+TYPED_TEST_CASE(fcv_basic_test, TypesUnderTest);
 
-TYPED_TEST(fcv_test, capacity)
+
+TYPED_TEST(fcv_basic_test, capacity)
 {
-	for(auto s : { 1, 2, 3, 7, 19, 23, 100, 1000, 1234, 1337, 8000 })
+	for(auto c : { 0, 1, 2, 3, 7, 19, 23, 100, 1000, 1234, 1337, 8000 })
 	{
-		TypeParam myvec(s);
-		EXPECT_EQ(s, myvec.capacity());
+		const TypeParam myvec(c);
+		ASSERT_EQ(c, myvec.capacity());
 	}
 }
+
+TYPED_TEST(fcv_basic_test, size)
+{
+	// testing size is not really possible without using functions like push_back and resize
+	// so we only test for size() = 0 on empty vector and for size() beeing callable on const objects
+	const std::size_t c = 8;
+	const TypeParam myvec(c);
+
+	ASSERT_EQ(0, myvec.size());
+}
+
+TYPED_TEST(fcv_basic_test, resize)
+{
+	const std::size_t c = 8;
+	TypeParam myvec(c);
+	ASSERT_EQ(0, myvec.size());
+
+	std::array<TypeParam::value_type, c> expected;
+
+	// test valid sizes (shrinking and growing)
+	auto data = myvec.data();
+	for(auto s : { 3, 7, 1, 8, 0, 4 })
+	{
+		// store current values in expected
+		auto oldSize = myvec.size();
+		for(std::size_t i=0; i<oldSize; ++i)
+			expected[i] = data[i];
+
+		// resize and fill with new size as value
+		ASSERT_NO_THROW(myvec.resize(s, construct<TypeParam::value_type>(s)));
+		
+		// check correct size
+		auto newSize = myvec.size();
+		ASSERT_EQ(s, newSize);
+
+		// check unchanged elements
+		ASSERT_EQ(true, std::equal(data, data + std::min(oldSize, newSize), begin(expected)));
+		// check new elements
+ 		for(std::size_t i=oldSize; i<newSize; ++i)
+ 			ASSERT_EQ(construct<TypeParam::value_type>(s), data[i]);
+	}
+}
+
+TYPED_TEST(fcv_basic_test, push_back_copy)
+{
+	const std::size_t c = 8;
+	TypeParam myvec(c);
+
+	std::array<TypeParam::value_type, c> expected;
+
+	// cases where vector is not full
+	for(std::size_t i=0; i<c; ++i)
+	{
+		expected[i] = construct<TypeParam::value_type>(i);
+
+		ASSERT_NO_THROW(myvec.push_back(expected[i]));
+		ASSERT_EQ(i+1, myvec.size());
+
+		auto data = myvec.data();
+		ASSERT_EQ(true, std::equal(data, data + i + 1, begin(expected)));
+	}
+
+	// cases where vector is full
+	const auto value = construct<TypeParam::value_type>(c);
+	ASSERT_THROW(myvec.push_back(value), std::length_error);
+	// check that size, capacity and content is unchanged
+	ASSERT_EQ(c, myvec.capacity());
+	ASSERT_EQ(c, myvec.size());
+	auto data = myvec.data();
+	ASSERT_EQ(true, std::equal(data, data + c, begin(expected)));
+}
+
+TYPED_TEST(fcv_basic_test, push_back_move)
+{
+	const std::size_t c = 8;
+	TypeParam myvec(c);
+
+	std::array<TypeParam::value_type, c> expected;
+
+	// cases where vector is not full
+	for(std::size_t i=0; i<c; ++i)
+	{
+		expected[i] = construct<TypeParam::value_type>(i);
+
+		ASSERT_NO_THROW(myvec.push_back(construct<TypeParam::value_type>(i)));
+		ASSERT_EQ(i+1, myvec.size());
+
+		auto data = myvec.data();
+		ASSERT_EQ(true, std::equal(data, data + i + 1, begin(expected)));
+	}
+
+	// cases where vector is full
+	ASSERT_THROW(myvec.push_back(construct<TypeParam::value_type>(c)), std::length_error);
+	// check that size, capacity and content is unchanged
+	ASSERT_EQ(c, myvec.capacity());
+	ASSERT_EQ(c, myvec.size());
+	auto data = myvec.data();
+	ASSERT_EQ(true, std::equal(data, data + c, begin(expected)));
+}
+
+TYPED_TEST(fcv_basic_test, at)
+{
+	const std::size_t c = 8;
+	TypeParam myvec(c);	
+	myvec.resize(c);
+	std::array<TypeParam::value_type, c> expected;
+	
+	// update via at
+	for(std::size_t i=0; i<c; ++i)
+	{
+		expected[i] = construct<TypeParam::value_type>(i);
+		myvec.at(i) = expected[i];
+	}
+	
+	auto data = myvec.data();
+	ASSERT_EQ(true, std::equal(data, data + c, begin(expected)));
+
+	// test read on const vector
+	const auto& myvec_c = myvec;
+	for(std::size_t i=0; i<c; ++i)
+	{
+		ASSERT_EQ(expected[i], myvec_c.at(i));
+	}
+}
+
+TYPED_TEST(fcv_basic_test, subscript_operator)
+{
+	const std::size_t c = 8;
+	TypeParam myvec(c);
+	myvec.resize(c);
+	std::array<TypeParam::value_type, c> expected;
+
+	// update via []
+	for(std::size_t i=0; i<c; ++i)
+	{
+		expected[i] = construct<TypeParam::value_type>(i);
+		myvec[i] = expected[i];
+	}
+
+	auto data = myvec.data();
+	ASSERT_EQ(true, std::equal(data, data + c, begin(expected)));
+
+	// test read on const vectors
+	const auto& myvec_c = myvec;
+	for(std::size_t i=0; i<c; ++i)
+	{
+		ASSERT_EQ(expected[i], myvec_c[i]);	
+	}
+}
+
 
 int main(int argc, char* argv[])
 {
--- gsoc_vector/vector.h
+++ gsoc_vector/vector.h
@@ -4,10 +4,15 @@
 #include <cstddef>
 #include <malloc.h>
 #include <exception>
+#include <vector>
+#include <cassert>
 
-#define FCV_NOEXCEPT 
+#ifdef _MSC_VER
+#define FCV_NOEXCEPT _NOEXCEPT
+#else
+#error "TODO!"
+#endif
 #define FCV_CONSTEXPR
-
 
 void* fcv_aligned_malloc(std::size_t alignment, std::size_t size)
 {
@@ -49,7 +54,7 @@ template<
 	typename _Ty,
 	typename _Alloc = std::allocator<_Ty>
 >
-class fixed_capacity_vector //: public _CapacityPolicy
+class fixed_capacity_vector
 {
 public:
 	typedef _Ty value_type;
@@ -57,15 +62,16 @@ public:
 	typedef unsigned int size_type;
 
 	fixed_capacity_vector(size_type _capacity)
-		: capacity_(_capacity)
+		: capacity_(_capacity), buffer_(nullptr)
 	{
-		buffer_ = static_cast<value_type*>(fcv_aligned_malloc(std::alignment_of<value_type>::value, 
-			capacity() * sizeof(value_type)));
+		buffer_ = allocator_.allocate(capacity_);
 	}
 
-	~fixed_capacity_vector()
+	~fixed_capacity_vector() FCV_NOEXCEPT
 	{
-		fcv_aligned_free(buffer_);
+		for(size_type i=0; i<size_; ++i)
+			allocator_.destroy(buffer_ + i);
+		allocator_.deallocate(buffer_, capacity_);
 	}
 
 	size_type capacity() const FCV_NOEXCEPT 
@@ -78,10 +84,84 @@ public:
 		return size_;
 	}
 
+	void resize(size_type _size, const value_type& value = value_type())
+	{
+		if(_size > capacity_)
+			throw std::length_error("size would exceed capacity of fixed_capacity_vector");
 
+		// shrink if new size is < current size
+		for(; size_ > _size; )
+		{
+			// destructors should not throw but in case one does, we still stay
+			// in a consistent state if we decrement size first
+			--size_; 
+			allocator_.destroy(buffer_ + size_);
+		}
 
+		// grow if new size is > current size
+		for(; size_ < _size; ++size_)
+		{
+			allocator_.construct(buffer_ + size_, value);
+			// increment size after construction because constructors may throw
+		}
+	}
+		
+	void push_back(const value_type& value)
+	{
+		if(size_ == capacity_)
+			throw std::length_error("fixed_capacity_vector out of capacity");
+
+		allocator_.construct(buffer_ + size_, value);
+		// modify size after copy to be consistent if copy-constructor throws
+		++size_;
+	}
+
+	void push_back(value_type&& value)
+	{
+		if(size_ == capacity_)
+			throw std::length_error("fixed_capacity_vector out of capacity");
+
+		allocator_.construct(buffer_ + size_, std::move(value));
+		// modify size after move to be consistent if move-constructor throws
+		++size_;
+	}
+
+	value_type& at(size_type index)
+	{
+		assert(index < size_);
+		return *(buffer_ + index);
+	}
+
+	const value_type& at(size_type index) const
+	{
+		assert(index < size_);
+		return *(buffer_ + index);
+	}
+
+	value_type& operator[](size_type index)
+	{
+		return at(index);
+	}
+
+	const value_type& operator[](size_type index) const
+	{
+		return at(index);
+	}
+	
+	value_type* data() FCV_NOEXCEPT
+	{
+		return buffer_;
+	}
+
+	const value_type* data() const FCV_NOEXCEPT
+	{
+		return buffer_;
+	}
+	
 private:
 	const size_type capacity_;
+	allocator_type allocator_;
+
 	value_type* buffer_;
 	size_type size_;
 };
